8da98df818d7a32dcd010afa7a37f07f04ba261c
Derick Rentz <derick.rentz@sap.com>
2020-06-11 16:50:09 -0300

    Removing code

--- src/pages/Day/index.tsx
+++ src/pages/Day/index.tsx
@@ -1,34 +1,21 @@
 import React, { Component } from 'react';
 
+import { DayInfo } from '../../models/DayInfo'
+
 import { Title } from './styles';
 
-type DayState = {
-    name: String;
+type DayProps = {
+    dayInfo: DayInfo;
 }
 
-type DayInfo = {
-    dayInfo: any;
-}
+export default class Day extends Component<DayProps, {}> {
 
-export default class Day extends Component<DayInfo, DayState> {
-
-constructor(props: DayInfo){
-    super(props);
-    console.log("teste" + props.dayInfo.dt)
-}
-
-    tick() {
-        this.setState({
-          name: "jefersan"
-        });
-    }
-
-    componentWillMount() {
-        this.tick();
+    constructor(props: DayProps){
+        super(props);
     }
 
     render() {
-        return <Title> {this.state.name} </Title>
+        return <Title> {this.props.dayInfo.id} </Title>
     }
 }
 
