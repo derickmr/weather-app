d14cc3a236230247231115c961ff6b121ca81d85
Derick Rentz <derick.rentz@sap.com>
2020-06-13 00:26:27 -0300

    Adding icon rendering inside card

--- src/pages/Day/index.tsx
+++ src/pages/Day/index.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import { DayInfo } from '../../models/DayInfo'
 
-import { Title } from './styles';
+import { Title, Card } from './styles';
 
 type DayProps = {
     dayInfo: DayInfo;
@@ -10,12 +10,18 @@ type DayProps = {
 
 export default class Day extends Component<DayProps, {}> {
 
+    iconUrl: string;
+
     constructor(props: DayProps){
         super(props);
+        this.iconUrl = `http://openweathermap.org/img/wn/${this.props.dayInfo.weather.icon}@2x.png`
     }
 
     render() {
-        return <Title> {this.props.dayInfo.id} </Title>
+        return <Card>
+                <Title>{this.props.dayInfo.temperature.day}</Title>
+                <img src={this.iconUrl} alt="new" />
+             </Card>
     }
 }
 
--- src/pages/Day/styles.tsx
+++ src/pages/Day/styles.tsx
@@ -5,3 +5,13 @@ export const Title = styled.h1`
     color: #7159c1;
     font-family: Arial, Helvetica, sans-serif;
 `;
+
+export const Card = styled.div`
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    float: left;
+    margin-right: 10px;
+    margin-bottom: 30px;
+    width: 100px;
+    height: auto;
+`;
