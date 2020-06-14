80b34d58c69def404f32af89ee3d88688592f79b
Derick Rentz <derick.rentz@sap.com>
2020-06-14 15:16:14 -0300

    Styling cards from initial page

--- src/pages/Day/styles.tsx
+++ src/pages/Day/styles.tsx
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 
+export interface TempTextProps {
+    color: string;
+}
+
 export const Title = styled.h1`
     font-size: 24px;
-    color: #7159c1;
+    color: #3F3F41;
     font-family: Arial, Helvetica, sans-serif;
+    padding: 10px;
 `;
 
 export const Card = styled.div`
@@ -12,6 +17,27 @@ export const Card = styled.div`
     float: left;
     margin-right: 10px;
     margin-bottom: 30px;
-    width: 100px;
+    width: 120px;
     height: auto;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding: 5px;
+`;
+
+export const Div = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+`;
+
+export const TempText = styled.p`
+    font-family: Arial, Helvetica, sans-serif;
+    color: ${(props: TempTextProps) => props.color};
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    font-size: 16px;
+    margin-top: 3px;
 `;
