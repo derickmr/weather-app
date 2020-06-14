c87aa61488eb0adc8f87efe71818b8e33402a6dd
Derick Rentz <derick.rentz@sap.com>
2020-06-14 15:14:23 -0300

    Adding global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GlobalStyle from '../src/styles/global'
 
 import Routes from './routes'
 
 function App() {
-    return <Routes/>;
+    return (
+        <>
+            <Routes/>
+            <GlobalStyle/>
+        </>
+        );
 }
 
 export default App;
